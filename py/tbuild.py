a4da887b523961b64608be208c2e742c763bd6e6
Arbeiter2 <petterbight@gmail.com>
2017-02-28 19:44:10 +0000

    Added datasources.py

--- py/tbuild.py
+++ py/tbuild.py
@@ -4,7 +4,9 @@ from TimetableBuilder import TimetableBuilder
 import pdb
 import os
 import sys, getopt
-
+from datasources import RESTDataSource, DBDataSource
+from flights import FlightManager
+from TimetableManager import TimetableManager
 
 types = {
     'F50' : 37, 
@@ -22,9 +24,22 @@ types = {
    
 def usage():
     print(
-    "Usage: {} [-g/game-id=] <game-id>\n"
+    "Usage: {}\n"
+    "Data source - database\n"
+      "\t\t[-H/--db-host=] <database-hostname>\n"
+      "\t\t[-D/--db-name=] <database-name>\n"
+      "\t\t[-U/--db-user=] <database-username>\n"
+      "\t\t[-P/--db-pass=] <database-password>\n"
+      
+    "\nData source - REST\n"
+      "\t\t[-u/--uri=] <URL-stub>\n"
+
+    "\nMandatory:\n"
+      "\t\t[-g/game-id=] <game-id>\n"
       "\t\t[-b/--base=] <base-iata-code>\n"
       "\t\t[-f/--fleet-type=] <icao fleet code> ({})\n"
+
+    "\nOptional\n"
       "\t\t[-s/--start-time] <HH:MM>\n"
       "\t\t[-t/--threshold=] <0.0000-0.9999>\n"
       "\t\t[-m/-max-range=] <max range allowed>\n"
@@ -47,16 +62,18 @@ def usage():
 if __name__ == '__main__':    
     try:
         opts, args = getopt.getopt(sys.argv[1:], 
-            "hg:b:f:s:t:m:c:d:rwiNQMj:x:G",
+            "hg:b:f:s:t:m:c:d:rwiNQMj:x:GH:D:U:P:u:",
             ["game-id=","base=", "fleet-type=", "start-time=", "threshold=", 
             "max-range=", "count=", "turnaround-delta=", "rebuild", "write",
-            "no-shuffle", "no-queue", "exclude=", "no-graveyard", "json"])
+            "no-shuffle", "no-queue", "exclude=", "no-graveyard", "json",
+            "db-host=", "db-name=", "db-user=", "db-pass=", "uri="])
     except getopt.GetoptError as err:
         print(err)  
         usage()
         sys.exit(2)
 
     game_id = base = fleet_type_id = None
+    db_host = db_name = db_user = db_pass = uri = None
     start_time = None
     base_turnaround_delta = None
     threshold = 0.99
@@ -77,6 +94,16 @@ if __name__ == '__main__':
         if o in ("-h", "--help"):
             usage()
             sys.exit()
+        elif o in ("-H", "--db-host="):
+            db_host = a
+        elif o in ("-D", "--db-name="):
+            db_name = a
+        elif o in ("-U", "--db-user="):
+            db_user = a
+        elif o in ("-P", "--db-pass="):
+            db_pass = a
+        elif o in ("-u", "--uri="):
+            uri = a
         elif o in ("-g", "--game-id"):
             game_id = a
         elif o in ("-b", "--base"):
@@ -124,15 +151,29 @@ if __name__ == '__main__':
             graveyard = False
         else:
             assert False, "unhandled option"
-
-    print(game_id, base, fleet_type_id, start_time, threshold, 
-        base_turnaround_delta, max_range, rebuild_all, writeToDatabase)
-    
+            
     if not game_id or not base or not fleet_type_id:
         usage()
         sys.exit()
         
-    builder = TimetableBuilder(game_id, shuffle=shuffle, use_rejected=use_rejected)
+    source = None
+    if (db_host and db_name and db_user and db_pass):
+        source = DBDataSource(game_id, db_host=db_host, db_name=db_name, 
+                              db_user=db_user, db_pass=db_pass)
+    elif (uri):
+        source = RESTDataSource(game_id, uri)
+    else:
+        raise Exception("No valid data sources provided")
+
+    #print(game_id, base, fleet_type_id, start_time, threshold, 
+    #    base_turnaround_delta, max_range, rebuild_all, writeToDatabase)
+
+    flightMgr = FlightManager(source)
+    timetableMgr = TimetableManager(source, flightMgr)
+    
+
+    builder = TimetableBuilder(flightMgr, timetableMgr, shuffle=shuffle, 
+                               use_rejected=use_rejected)
     
     try:
         builder(base_airport_iata=base, fleet_type_id=fleet_type_id, 
